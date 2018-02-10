2afb1be0864b5c83c9ca855e218f8ba390cca1dd
Oriel <orielux@gmail.com|git config --global user.email orielux@gmail.comcommit Bug>
2018-02-10 18:30:45 +0200

    Small refactor + progress logs

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const fs = require('fs-extra');
 const unzip = require('unzip');
 const os = require('os');
 const chokidar = require('chokidar');
+const emoji = require('node-emoji')
 
-const {idle, getMostRecentFileName} = require('./utils')
+const {idle, getMostRecentFileName, catchAsyncErrors, log} = require('./utils')
 const { URLS, SELECTORS } = require('./consts');
 const {ICOMOON_USER, ICOMOON_PASS, FONT_NAME, ICONS_FILE_PATH, ICONS_FONTS_DIR, OVERWRITE_FONTS_PATH} = process.env;
 const DOWNLOADS_DIR = path.join(os.homedir(), 'Downloads');
@@ -14,16 +15,15 @@ const EXTRACTED_ICONS_FILE = path.join(EXTRACTED_FONTS_DIR, 'style.css');
 
 (async () => {
   
-  fs.ensureDirSync(DOWNLOADS_DIR);
-
-  const browser = await puppeteer.launch();
+  await fs.ensureDir(DOWNLOADS_DIR);
+  
+  const browser = await puppeteer.launch({haedless: false});
   const page = await browser.newPage();
 
   await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: DOWNLOADS_DIR});
 
-  await page.goto(URLS.login);
-
-  const submitButton = await page.waitForSelector(SELECTORS.submitButton, {visible: true});
+  await catchAsyncErrors(() => page.goto(URLS.login))
+  const submitButton = await catchAsyncErrors(() => page.waitForSelector(SELECTORS.submitButton, {visible: true}));
   
   const emailField = await page.$(SELECTORS.emailInput);
   const passField = await page.$(SELECTORS.passwordInput);
@@ -38,53 +38,55 @@ const EXTRACTED_ICONS_FILE = path.join(EXTRACTED_FONTS_DIR, 'style.css');
   
   await page.waitForFunction(() => window.location.href === 'https://icomoon.io/#profile');
 
-  await page.goto(URLS.select);
-  
-  (await page.waitForSelector(SELECTORS.selectFont, {visible: true})).click();
-  
-  page.goto(URLS.selectFont, {timeout: 0});
+  log(`Logged in to icomoon`);
 
-  const numOfFilesBeforeDownload = fs.readdirSync(DOWNLOADS_DIR).length;
+  await catchAsyncErrors(() => page.goto(URLS.select));
   
-  (await page.waitForSelector(SELECTORS.downloadFont, {visible: true})).click();
+  (await catchAsyncErrors(() => page.waitForSelector(SELECTORS.selectFont, {visible: true}))).click();
+  
+  catchAsyncErrors(() => page.goto(URLS.selectFont, {timeout: 0}));
+
+  const numOfFilesBeforeDownload = await catchAsyncErrors(() => fs.readdir(DOWNLOADS_DIR).length, `Unable to access ${DOWNLOADS_DIR}`);
+  
+  (await catchAsyncErrors(() => page.waitForSelector(SELECTORS.downloadFont, {visible: true}))).click();
+  
+  log(`Downloading fonts...`);
 
   await new Promise((resolve, reject) => {
-    const watcher = fs.watch(DOWNLOADS_DIR, { encoding: 'buffer' }, (eventType, filename) => {
-      if (filename) {
-        watcher.close();
-        resolve();
-      }
+    const watcher = fs.watch(DOWNLOADS_DIR, { encoding: 'buffer' }, (e, filename) => { 
+      if (filename) { 
+        watcher.close(); 
+        resolve(); 
+      } 
     });
-  })
+  });
+  
+  log(`Fonts downloaded`);
 
   const mostRecentFileName = getMostRecentFileName(DOWNLOADS_DIR);
   const mostRecentFilePath = path.join(DOWNLOADS_DIR, mostRecentFileName);
 
   fs.createReadStream(mostRecentFilePath)
     .pipe(unzip.Extract({ path: EXTRACTED_FONTS_DIR }));
+
+  log(`Fonts extracted to ${EXTRACTED_FONTS_DIR}`);
+
+  let content = await fs.readFile(EXTRACTED_ICONS_FILE, 'utf8');
   
-  fs.readFile(EXTRACTED_ICONS_FILE, 'utf8', (err,data) => {
-    
-    if (err) {
-      return console.log('Error reading file', err);
-    }
+  if (OVERWRITE_FONTS_PATH) {
+    content = content.replace(/fonts/g, OVERWRITE_FONTS_PATH);
+  }
 
-    if (OVERWRITE_FONTS_PATH) {
-      var result = data.replace(/fonts/g, OVERWRITE_FONTS_PATH);
-    }
+  await fs.writeFile(ICONS_FILE_PATH, content);
 
-    fs.writeFile(ICONS_FILE_PATH, result, 'utf8', (err) => {
-       if (err) return console.log('Error writing file', err);
-    });
+  await fs.copy(path.join(EXTRACTED_FONTS_DIR, 'fonts'), ICONS_FONTS_DIR)
 
-  });
-
-  fs.copySync(path.join(EXTRACTED_FONTS_DIR, 'fonts'), ICONS_FONTS_DIR)
+  log(`Fonts copied to ${ICONS_FONTS_DIR}`);
 
   await browser.close();
 
-  console.log(`Fonts extracted to ${ICONS_FONTS_DIR}`);
-
-  return;
+  log(`${emoji.get('thumbsup')}`);
   
+  process.exit(0);
+
 })();
--- utils.js
+++ utils.js
@@ -5,8 +5,12 @@ const {setTimeout} = require('timers');
 
 const idle = function(ms) { return new Promise((resolve, reject) => setTimeout(resolve, ms)) };
 const getMostRecentFileName = (dir) => _.max(fs.readdirSync(dir), (f) => fs.statSync(path.join(dir, f)).ctime);
+const catchAsyncErrors = (fn, errMessage) => { try { return fn() } catch(e) { if (errMessage) throw new Error(errMessage, e) } };
+const log = (message) => console.log(`[icomoon-fetcher] ${message}`);
 
 module.exports = {
     idle,
-    getMostRecentFileName    
+    getMostRecentFileName,
+    catchAsyncErrors,
+    log
 }
